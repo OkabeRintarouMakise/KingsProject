f998bdb9bda2d607a72b73ca3b2f92b30551738c
Harun Abukar <k21036507@kcl.ac.uk>
2023-03-14 13:56:21 +0000

    changes to StatisticsPanel

--- StatisticsPanel.java
+++ StatisticsPanel.java
@@ -4,11 +4,13 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.geometry.Insets;
+import javafx.geometry.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.*;
+
 
 /**
  * Write a description of JavaFX class StatisticsPanel here.
@@ -18,10 +20,9 @@ import javafx.stage.Stage;
  */
 public class StatisticsPanel extends Application
 {
-    // We keep track of the count, and label displaying the count:
-    private int count = 0;
-    private Label myLabel = new Label("0");
-
+    Button leftButton = new Button();
+    Button rightButton = new Button();
+    Label middleLabel = new Label("statistics: ");
     /**
      * The start method is the main entry point for every JavaFX application. 
      * It is called after the init() method has returned and after 
@@ -32,25 +33,33 @@ public class StatisticsPanel extends Application
     @Override
     public void start(Stage stage)
     {
-        // Create a Button or any control item
-        Button myButton = new Button("Count");
+        BorderPane bPane = new BorderPane();
+     
+        leftButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        rightButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        leftButton.setPadding(new Insets(50, 10, 50, 10));
+        rightButton.setPadding(new Insets(50, 10, 50, 10));
+        leftButton.setText("<");
+        rightButton.setText(">");
+        
+        middleLabel.setPadding(new Insets(50, 50, 50, 50));
+        
 
-        // Create a new grid pane
-        GridPane pane = new GridPane();
-        pane.setPadding(new Insets(10, 10, 10, 10));
-        pane.setMinSize(300, 300);
-        pane.setVgap(10);
-        pane.setHgap(10);
+        BorderPane.setMargin(leftButton, new Insets(2,5,2,5));
+        BorderPane.setMargin(rightButton, new Insets(2,5,2,5));
 
-        //set an action on the button using method reference
-        myButton.setOnAction(this::buttonClick);
-
-        // Add the button and label into the pane
-        pane.add(myLabel, 1, 0);
-        pane.add(myButton, 0, 0);
+        
+   
+        
+        
+        bPane.setLeft(leftButton);
+        bPane.setCenter(middleLabel);
+        bPane.setRight(rightButton);
+        
+        
 
         // JavaFX must have a Scene (window content) inside a Stage (window)
-        Scene scene = new Scene(pane, 300,100);
+        Scene scene = new Scene(bPane, 800, 500);
         stage.setTitle("JavaFX Example");
         stage.setScene(scene);
 
@@ -58,14 +67,5 @@ public class StatisticsPanel extends Application
         stage.show();
     }
 
-    /**
-     * This will be executed when the button is clicked
-     * It increments the count by 1
-     */
-    private void buttonClick(ActionEvent event)
-    {
-        // Counts number of button clicks and shows the result on a label
-        count = count + 1;
-        myLabel.setText(Integer.toString(count));
-    }
+
 }
