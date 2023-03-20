16402cfeecd7fc5814834961d840b579be50c926
Adil Kassam <K22004842@kcl.ac.uk>
2023-03-20 18:51:06 +0000

    From and To conditions

--- CovidDataGUI.java
+++ CovidDataGUI.java
@@ -1,5 +1,4 @@
 
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,6 +15,7 @@ import java.util.List;
 import java.util.*;
 import javafx.scene.control.*;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert.AlertType;
 
 /**
  * Write a description of JavaFX class CovidDataGUI here.
@@ -30,10 +30,10 @@ public class CovidDataGUI extends Application
     Button rightButton = new Button();
     CovidDataLoader dateFetcher  = new CovidDataLoader();
     private HashSet<String> dateCollection = new HashSet<>();
+    ArrayList<String> orderedDates = new ArrayList<String>();
     ComboBox from = new ComboBox();
     ComboBox to = new ComboBox();
 
-    
     /**
      * The start method is the main entry point for every JavaFX application. 
      * It is called after the init() method has returned and after 
@@ -44,17 +44,14 @@ public class CovidDataGUI extends Application
     @Override
     public void start(Stage stage)
     {
-        
+
         this.stage = stage;
-        
+
         dateFetcher.load();
         collectionLoader(from);
         collectionLoader(to);
         Label fromLabel = new Label("From");
         Label toLabel = new Label("To");
-        
-
-        
 
         
         
@@ -65,38 +62,27 @@ public class CovidDataGUI extends Application
         hbox.getChildren().add(from);
         hbox.getChildren().add(toLabel);
         hbox.getChildren().add(to);
-        
+
         AnchorPane topAnchorPane = new AnchorPane();
         AnchorPane bottomAnchorPane = new AnchorPane();
-        
+
         borderPane.setTop(hbox);
         borderPane.setCenter(topAnchorPane);
         borderPane.setBottom(bottomAnchorPane);
-        
+
         leftButton.setText("<");
         leftButton.setMaxWidth(Double.MAX_VALUE);
-        
+
         rightButton.setText(">");
         rightButton.setMaxWidth(Double.MAX_VALUE);
         
+        disableButtons(true);
+
         AnchorPane.setLeftAnchor(leftButton, 0d);
         AnchorPane.setRightAnchor(rightButton, 0d);
         bottomAnchorPane.getChildren().addAll(leftButton, rightButton);
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
 
+        
         // JavaFX must have a Scene (window content) inside a Stage (window)
         Scene scene = new Scene(borderPane, 550, 400);
         stage.setScene(scene);
@@ -104,12 +90,22 @@ public class CovidDataGUI extends Application
         // Show the Stage (window)
         stage.show();
     }
-    
+
+    /*private void collectionLoader(ComboBox combo){
+        for(CovidData record : dateFetcher.getData()){
+            orderedDates.add(record.getDate());
+        }
+        Collections.sort(orderedDates);
+        for(String date: orderedDates){
+            combo.getItems().add(date);
+        }
+    }
+    */
     private void collectionLoader(ComboBox combo){
         for(CovidData record : dateFetcher.getData()){
             dateCollection.add(record.getDate());
         }
-        List<String> orderedDates = new ArrayList<String>(dateCollection);
+        orderedDates.addAll(dateCollection);
         Collections.sort(orderedDates);
         for(String date: orderedDates){
             combo.getItems().add(date);
@@ -117,12 +113,34 @@ public class CovidDataGUI extends Application
     }
     
     
-    private void setUpMenu(Pane parent){
+    private void dropDownBoxConditions()
+    {
+        String currentFromValue = (String) from.getValue();
+        String currentToValue = (String) to.getValue();
+        if(orderedDates.indexOf(currentFromValue) > orderedDates.indexOf(currentToValue)){
+            dropDownError();
+            return;
+        }else{
+            disableButtons(false);
+        }
         
-        
+    }
+    
+    private void dropDownError()
+    {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Display Condition Error");
+        alert.setHeaderText(null);
+        alert.setContentText("Your From selection is greater than your To selection");
+        alert.showAndWait();
+    }
     
     
+
+    private void disableButtons(boolean state)
+    {
+        leftButton.setDisable(state);
+        rightButton.setDisable(state);
     }
 
-    
 }
