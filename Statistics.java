117a46ad812976e9dca69868fcb27bf2dfe927d6
harun abukar <k21036507@kcl.ac.uk>
2023-03-21 14:15:32 +0000

    addition to statistics and more styling added

--- Statistics.java
+++ Statistics.java
@@ -15,30 +15,26 @@ public class Statistics
     //instance of the class CovidDataLoader so that we can import data
     CovidDataLoader statisticsLoader = new CovidDataLoader();
 
-
-    ArrayList<Date> dateRange = new ArrayList<>();
     private String first;
     private String last;
     CovidDataGUI mainGUI = new CovidDataGUI();
-  
-
 
     /**
      * Constructor for objects of class Statistics
      */
     public Statistics()
     {
-        //setDates();
+        setDates();
         statisticsLoader.load();
 
         allData = statisticsLoader.getData();
         Collections.sort(allData, new SortByDate());
-        
+
         statList.add("Total deaths in all London boroughs: \n" + loadTotalDeaths());
         statList.add("Average cases per London borough: \n" + averageCases());
-        statList.add("statistic 3");
-        statList.add("statistic 4");
-        statList.add("statistic 5");
+        statList.add("Average parks GMR: \n" + averageParksGMR() + "%");
+        statList.add("Average Workplaces GMR: \n" + averageWorkplacesGMR() + "%");
+        statList.add("This date has the highest \n number of total deaths \n" + getHighestDeathDate());
 
     }
 
@@ -63,15 +59,15 @@ public class Statistics
     private int loadTotalDeaths(){
 
         int totalLondonDeaths = 0;
-        
+
         for(int i = 0; i < allData.size(); i++){
             if(first == null || last == null){
                 totalLondonDeaths += allData.get(i).getNewDeaths();
             }
-            
+
             else if(i >= allData.indexOf(first) && i <= allData.indexOf(last)){
-            totalLondonDeaths += allData.get(i).getNewDeaths();
-        }
+                totalLondonDeaths += allData.get(i).getNewDeaths();
+            }
         }
         return totalLondonDeaths;
     }
@@ -83,24 +79,74 @@ public class Statistics
     private int averageCases(){
 
         int averageCases = 0;
-        
+
         for(int i = 0; i < allData.size(); i++){
             if(first == null || last == null){
                 averageCases += allData.get(i).getNewCases();
             }
             else if(i >= allData.indexOf(first) && i <= allData.indexOf(last)){
-            averageCases += allData.get(i).getNewCases();
+                averageCases += allData.get(i).getNewCases();
+            }
         }
-        }
-        
+
         averageCases = averageCases / 33;
         return averageCases;
 
     }
-    /* private void setDates(){
-    first = mainGUI.getFrom();
-    last = mainGUI.getTo();
+
+    private int averageParksGMR(){
+
+        int averageParksGMR = 0;
+
+        for(int i = 0; i < allData.size(); i++){
+            if(first == null || last == null){
+                averageParksGMR += allData.get(i).getParksGMR();
+            }
+            else if(i >= allData.indexOf(first) && i <= allData.indexOf(last)){
+                averageParksGMR += allData.get(i).getParksGMR();
+            }
+        }
+        averageParksGMR = averageParksGMR / allData.size();
+        return averageParksGMR;
+
     }
-    */
+
+    private int averageWorkplacesGMR(){
+
+        int averageWorkplacesGMR = 0;
+
+        for(int i = 0; i < allData.size(); i++){
+            if(first == null || last == null){
+                averageWorkplacesGMR += allData.get(i).getWorkplacesGMR();
+            }
+            else if(i >= allData.indexOf(first) && i <= allData.indexOf(last)){
+                averageWorkplacesGMR += allData.get(i).getWorkplacesGMR();
+            }
+        }
+        averageWorkplacesGMR = averageWorkplacesGMR / allData.size();
+        return averageWorkplacesGMR;
+
+    }
+
+    private String getHighestDeathDate(){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(CovidData data: allData){
+            arrayList.add(data.getTotalDeaths());
+        }
+
+        for(CovidData data: allData){
+            int i = Collections.max(arrayList);
+            if(data.getTotalDeaths() == i){
+                return data.getDate();
+            }
+        }
+        return "";
+    }
+
+    private void setDates(){
+        first = mainGUI.getFrom();
+        last = mainGUI.getTo();
+    }
+
 }
 
